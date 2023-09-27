2abab2ae6fef247af5d8f82246e2d450e82b831e
efrinbabu <efrinbabuk1999@gmail.com>
2023-09-27 14:08:14 +0530

    New Product controller add

---
+++ src/Controller/ContentController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Pimcore\Controller\FrontendController;
+use Symfony\Bridge\Twig\Attribute\Template;
+use Symfony\Component\HttpFoundation\Request;
+
+class ContentController extends FrontendController
+{
+    #[Template('content/default.html.twig')]
+    public function defaultAction(Request $request): array
+    {
+        return [];
+    }
+
+    public function movieAction(Request $request)
+    {
+        return $this->render('content/movie.html.twig');
+    }
+}
